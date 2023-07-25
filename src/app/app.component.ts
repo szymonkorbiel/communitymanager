638c3144f057c55ed7b1173777b10b89f4f4d672
szymonkorbiel <szymon.korbiel@gmail.com>
2023-07-25 22:42:08 +0200

    Add new files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,10 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
+  
 })
 export class AppComponent {
   title = 'CommunityManager';
+  const DEFAULT_USER = { name: 'Jan Niezbędny' };
 }
+
